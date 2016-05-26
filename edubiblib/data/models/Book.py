b1915eaffb3744016a0a7c031fd9913c86bc7e92
Daniel Laube <github@dlaube.de>
2016-05-26 21:38:43 +0200

    More classes. Bens todo

---
+++ edubiblib/data/models/Book.py
@@ -0,0 +1,2 @@
+# TODO: Ben
+# Implement a book class for one instance of a book. Think of all required attributes
---
+++ edubiblib/data/models/Person.py
@@ -0,0 +1,2 @@
+# TODO: Ben
+# Implement a class for one Person/Individual. Think of all attributes that are relevant
