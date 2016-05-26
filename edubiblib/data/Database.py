bdbe32428ef3bc80e77c5b53ecc61592eba9fbdf
Daniel Laube <github@dlaube.de>
2016-05-26 21:17:25 +0200

    Project structure part 2

--- edubiblib/data/Database.py
+++ edubiblib/data/Database.py
@@ -1,5 +1,6 @@
 import sqlite3
-from models import BookTemplate
+from models.BookTemplate import BookTemplate
+from models.Merchant import Merchant
 
 
 class Database(object):
@@ -11,7 +12,7 @@ class Database(object):
         """
         Constructor, initialize the database connection to a local database
         """
-        self.connection = sqlite3.connection("datenbank.sqlite")
+        self.connection = sqlite3.connection("eduBib.sqlite")
         self.checkTables()
 
     def checkTables(self):
@@ -28,9 +29,22 @@ class Database(object):
         :return: if found: book template with the specified id, else None
         """
         cursor = self.connection.cursor()
-        res = cursor.execute("SELECT id, isbn, name, autor, verlag FROM buecher_template WHERE id = '?';", id)
+        cursor.execute("SELECT id, isbn, name, autor, verlag FROM buecher_template WHERE id = '?';", id)
         data = cursor.fetchone()
 
         bookTemplate = BuchTemplate(data[0], data[1], data[2], data[3], data[5])
         cursor.close()
-        return bookTemplate
+        return bookTemplate # TODO: Return None if no book template with given id
+
+    def getMerchant(self, id):
+        """
+        Returns a merchant with given id
+        :param id: int - the id to search for
+        :return: if found: merchant with specific id, else None
+        """
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT id, name, address FROM merchants WHERE id = '?';", id)
+        data = cursor.fetchone()
+
+        merchant = Merchant(data[0], data[1], data[2])
+        return merchant # TODO: Return None if no merchant with given id
---
+++ edubiblib/data/models/Merchant.py
@@ -0,0 +1,34 @@
+
+
+class Merchant(object):
+    """
+    Merchant database model class. Contains all data that belongs to a merchant.
+    """
+    def __init__(self, id, name, address):
+        """
+        Constructor prefilling all attributes
+        """
+        self._id = id
+        self._name = name
+        self._address = address
+
+    def getID(self):
+        """
+        Getter for id
+        :return: return merchant id
+        """
+        return self._id
+
+    def getName(self):
+        """
+        Getter for name
+        :return: returns merchant id
+        """
+        return self._name
+
+    def getAdress(self):
+        """
+        Getter for address
+        :return: returns merchant adress
+        """
+        return self._address
