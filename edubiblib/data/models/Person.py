dc500dd440f15fc8cf181428d9a1714a5587e756
BenMoebius <moebiub@ratsgymnasium-pe.de>
2016-05-27 17:36:13 +0200

    Update Person.py

--- edubiblib/data/models/Person.py
+++ edubiblib/data/models/Person.py
@@ -1,2 +1,15 @@
 # TODO: Ben
 # Implement a class for one Person/Individual. Think of all attributes that are relevant
+class Person(object):
+	def __init__(self,id_, name, klasse)
+		self.id = id_
+		self.name = name
+		self.klasse = klasse
+	
+	def getID(self):
+		return self.id
+	def getName(self):
+		return self.name
+	def getKlasse(self):
+		return self.klasse
+
