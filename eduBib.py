e7c9a219c54c6e8a9d189e309d17d780bd07876c
Daniel Laube <c9@dlaube.de>
2016-05-28 17:10:56 +0000

    Added some example display code

--- eduBib.py
+++ eduBib.py
@@ -3,8 +3,14 @@ Main File
 """
 
 from edubiblib.data.Database import database
+from Tkinter import Tk
 
 if __name__ == "__main__":
     """
     Main entry. Start the application here
     """
+    tkFenster = Tk()
+    tkFenster.title('eduBib - Hub')
+    tkFenster.geometry('350x145')
+    # Aktivierung des Fensters
+    tkFenster.mainloop()
