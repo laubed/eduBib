40dd626e3e49ec668cc6a815a303a313d5763ff0
BenMoebius <moebiub@ratsgymnasium-pe.de>
2016-07-14 19:58:56 +0200

    homescreen

---
+++ misc/home.py
@@ -0,0 +1,57 @@
+#imports
+import sys
+import os
+from PyQt4 import QtGui, QtCore
+
+class Window(QtGui.QWidget):
+    
+    def __init__(self):
+        
+  
+        QtGui.QWidget.__init__(self)
+        self.layout =  QtGui.QVBoxLayout()
+        self.setWindowTitle('eduBib')
+        self.setGeometry(2000,200,500,182)
+        self.setWindowIcon(QtGui.QIcon('eduBibLogo.png'))
+        self.header = QtGui.QLabel(self)
+        text = None
+        self.header.setPixmap(QtGui.QPixmap(os.getcwd()+"/header2.png"))
+        self.hwidth = 400
+        self.hheight=157
+        self.header.setGeometry(0,0,self.hwidth,self.hheight)
+        self.inven=self.addbutton('Inventory',100,10,0,158)
+        self.inven.clicked.connect(self.inven_function)
+        self.borrow =self.addbutton('Borrow',100,10,100,158)
+        self.admin=self.addbutton('Administration',100,10,200,158)
+        self.acqui=self.addbutton('Acquisition',100,10,300,158)
+        self.quit=self.addbutton('Quit',100,10,400,158)
+        self.options = self.addbutton('Options',100,10,400,0)
+        self.credits = self.addbutton('Credits',100,10,400,22)
+        self.imprint = self.addbutton('Imprint',100,10,400,44)
+        self.copyright = self.addbutton('Help',100,10,400,66)
+        self.show()
+    def run(self):
+        
+        app = QtGui.QApplication(sys.argv)
+        app.exec_()
+        self.show()
+        sys.exit(app.exec_())
+    def addText(self,text):
+        self.text = QtGui.QLabel(text,self)
+        self.text.setFont(QtGui.QFont("Arial", 32))
+        self.a = 500
+        self.b= 600
+        
+        self.text.setGeometry(350,0,self.a,self.b)
+    def addbutton(self,text,width,height,x,y):
+        
+        button = QtGui.QPushButton(text,self)
+        button.move(x,y)
+        button.setFixedWidth(width)
+        self.layout.addWidget(button)
+        return button
+    def inven_function(self):
+        print('Inventory Button')
+app = QtGui.QApplication(sys.argv)
+window = Window()
+window.run()
